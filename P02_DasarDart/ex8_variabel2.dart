8aed802aed18416601e3751f037a67f4a1a22852
Hastita Sari <hastitasari@gmail.com>
2025-03-10 13:34:00 +0700

    Update ex8_variabel2.dart

--- P02_DasarDart/ex8_variabel2.dart
+++ P02_DasarDart/ex8_variabel2.dart
@@ -1,11 +1,11 @@
 //nilai default variabel
 void main(List<String> args) {
     int? x;  // Menggunakan ? untuk mengizinkan int bernilai null
-    print(x == null);
+    print(x is Null);
     print(x);
 
     x = 1;
-    print(x == null);
+    print(x is Null);
     print(x is int);
     print(x);
 }
